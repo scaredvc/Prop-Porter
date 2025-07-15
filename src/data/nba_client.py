86ee732197400445d6f56e36bd0da5698bf278d9
Hanson <lauhanson318@gmail.com>
2025-07-14 23:57:55 -0700

    added function to search for players by id, season and season type

--- src/data/nba_client.py
+++ src/data/nba_client.py
@@ -1,11 +1,15 @@
 from nba_api.stats.static import teams
-
+from nba_api.stats.endpoints import playergamelog
 def get_all_teams():
     return teams.get_teams()
 
+def get_game_logs_for_player(player_id, season, season_type):
+    return playergamelog.PlayerGameLog(player_id=player_id, season=season, season_type_all_star=season_type)
+
 def main():
     teams = get_all_teams()
-    print(teams)
+    game_logs = get_game_logs_for_player(237, '2023-24', 'Regular Season')
+    print(game_logs.get_data_frames()[0])
 
 if __name__ == "__main__":
     main()
