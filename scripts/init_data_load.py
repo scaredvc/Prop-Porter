e1b133db2b1aa6c5f37dee6836b0039df5b556e6
Hanson <lauhanson318@gmail.com>
2025-07-16 02:34:12 -0700

    added init data loading framework

---
+++ scripts/init_data_load.py
@@ -0,0 +1,46 @@
+import psycopg2
+import os
+from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import playergamelog
+import time
+
+connection = psycopg2.connect(
+    dbname = "postgres",
+    user = "postgres",
+    password = "beattheodds",
+    host = "localhost",
+    port = "5432",
+)
+
+cur = connection.cursor()
+print("connected to database")
+
+def load_players_data():
+    all_players = players.get_players()
+    print(f'Retrieving {len(all_players)} players')
+
+    for player in all_players:
+        player_id = player["id"]
+        player_full_name = player["full_name"]
+        player_first_name = player["first_name"]
+        player_last_name = player["last_name"]
+        player_active_status = player["is_active"]
+
+        sql_command = "INSERT INTO players (id, full_name, first_name, last_name, is_active) VALUES (%s,%s,%s,%s,%s)"
+        values_to_insert = (player_id, player_full_name, player_first_name, player_last_name, player_active_status)
+
+        cur.execute(sql_command, values_to_insert)
+
+        
+        pass
+
+    connection.commit()
+    print("Done loading players")
+
+if __name__ == "__main__":
+
+    load_players_data()
+
+    cur.close()
+    connection.close()
+    print("Connection to database closed")
