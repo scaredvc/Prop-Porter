6d3e8db48eb0f5f74d5bafdeaa60f37ae657bce7
Hanson <lauhanson318@gmail.com>
2025-07-20 17:58:25 -0700

    feat (api): added /get_player_stats endpoint

--- src/api/server.py
+++ src/api/server.py
@@ -1,6 +1,6 @@
 import psycopg2
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, 
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,9 +27,12 @@ def get_teams():
         cur = conn.cursor()
 
         cur.execute("""
-                    SELECT id, full_name, abbreviation, nickname, city, state, year_founded 
-                    FROM teams 
-                    ORDER BY full_name;
+                    SELECT id, 
+                        full_name, abbreviation, nickname, city, state, year_founded 
+                    FROM 
+                        teams 
+                    ORDER BY 
+                        full_name;
                     """)
         result = cur.fetchall()
 
@@ -59,9 +62,12 @@ def get_player():
         cur = conn.cursor()
 
         cur.execute("""
-                    SELECT id, full_name, first_name, last_name, is_active
-                    FROM players 
-                    ORDER BY full_name
+                    SELECT 
+                        id, full_name, first_name, last_name, is_active
+                    FROM 
+                        players 
+                    ORDER BY 
+                        full_name
                     """)
         
         result = cur.fetchall()
@@ -81,3 +87,46 @@ def get_player():
             conn.close()
         
         return jsonify(player_list)
+
+@app.route("/api/v1/players/<int:player_id>/stats", methods = ["GET"])
+def get_player_stats(player_id):
+    
+    player_stats = []
+    conn = None
+
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        sql_query = ( """
+            SELECT 
+                id, player_id, game_id, team_id, minutes, points, 
+                rebounds, assists, steals, blocks, turnovers, fgm, 
+                fga, fg_pct, fg3m, fg3a, fg3_pct, ftm, fta, ft_pct
+            FROM 
+                player_game_stats
+            WHERE 
+                player_id = %s
+            ORDER BY 
+                game_date;
+            """)
+        
+        cur.execute(sql_query, (player_id,))
+            
+        result = cur.fetchall()
+
+        if result and cur.description:
+            columns = (desc[0] for desc in cur.description)
+            player_stats = [dict(zip(columns,row)) for row in result]
+
+        cur.close()
+
+    except Exception as e:
+        print(e)
+        player_stats = []
+
+    finally: 
+        if conn is not None:
+            conn.close()
+
+        return jsonify(player_stats)
