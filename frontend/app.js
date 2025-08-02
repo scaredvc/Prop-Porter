da23071b61ff4b233c7f21185b01b408b66c102f
Hanson <lauhanson318@gmail.com>
2025-08-01 21:29:56 -0700

    working front end connecting with api endpoints for player point prediction against team

--- frontend/app.js
+++ frontend/app.js
@@ -53,3 +53,39 @@ function populateDropdown(selectElement, data, nameKey, valueKey) {
     console.log("app initialized");
 
 })();
+
+predictButton.addEventListener('click', async() =>{
+    console.log("button is clicked");
+
+    const selectedPlayerId = playerSelect.value;
+    const selectedTeamId = teamSelect.value;
+
+    console.log("Selected Player ID:", selectedPlayerId);
+    console.log("Selected Team ID:", selectedTeamId);
+
+    if(selectedPlayerId === "" || selectedTeamId === "") return; 
+    resultContainer.innerHTML = `<h2>Asking Porter...</h2>`
+
+    try{
+        const fullUrl = `${API_BASE_URL}/predict?player_id=${selectedPlayerId}&opponent_team_id=${selectedTeamId}`;
+        console.log("Fetching URL:", fullUrl);
+        const response = await fetch(fullUrl);
+
+        if (!response.ok){
+            throw new Error(`prediction api failed ${response.status}`);
+        }
+        const predictionData = await response.json();
+
+        const predicted_points = predictionData.predicted_points;
+
+        if (predicted_points !== undefined){
+            resultContainer.innerHTML = `<h2>predicted points: <span class = "prediction-value">${predicted_points}</span></h2>`;
+        } else {
+            throw new Error(`Prediction data is missing`);
+        }
+    }
+    catch (error){
+        console.error(`error during prediction`, error)
+        resultContainer.innerHTML = `<h2>Error getting prediction please try again</h2>`
+    }
+})
